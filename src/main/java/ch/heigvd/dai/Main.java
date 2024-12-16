175160d6dba099562486b9ddd565a34b40c1ad85
Mondotosz <git@moncorp.ch>
2024-12-16 12:04:57 +0100

    updated to kind of work with picocli

--- src/main/java/ch/heigvd/dai/Main.java
+++ src/main/java/ch/heigvd/dai/Main.java
@@ -3,6 +3,7 @@ package ch.heigvd.dai;
 import java.io.File;
 import java.util.concurrent.Callable;
 
+import io.javalin.Javalin;
 import picocli.CommandLine;
 
 @CommandLine.Command(description = "A small CLI with subcommands to demonstrate picocli.", version = "1.0.0", subcommands = {
@@ -10,12 +11,13 @@ import picocli.CommandLine;
 }, scope = CommandLine.ScopeType.INHERIT, mixinStandardHelpOptions = true)
 public class Main implements Callable<Integer> {
 
-  @CommandLine.Parameters(index = "0", description = "The name of the user (default: World).", defaultValue = "World")
-  protected String name;
+  @CommandLine.Option(names = { "-a",
+      "--address" }, description = "The address to listen on", defaultValue = "localhost")
+  private String address;
 
-  public String getName() {
-    return this.name;
-  }
+  @CommandLine.Option(names = { "-p",
+      "--port" }, description = "The port to listen on when using server mode", defaultValue = "8080")
+  private int port;
 
   public static void main(String[] args) {
     String jarFilename =
@@ -23,14 +25,19 @@ public class Main implements Callable<Integer> {
         new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath())
             .getName();
 
-    int exitCode = new CommandLine(new Main()).setCommandName(jarFilename).execute(args);
+    Integer code = new CommandLine(new Main()).setCommandName(jarFilename).execute(args);
 
-    System.exit(exitCode);
+    if (code != 0) {
+      System.exit(code);
+    }
+
   }
 
   @Override
   public Integer call() throws Exception {
-    System.out.println("Hello world");
+    var app = Javalin.create(/* config */)
+        .get("/", ctx -> ctx.result("Hello World"))
+        .start(port);
     return 0;
   }
 }
