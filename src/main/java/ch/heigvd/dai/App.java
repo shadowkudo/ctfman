cdb8e3c3d2fe1f45dd28c256286400abce944e04
Mondotosz <git@moncorp.ch>
2024-12-16 13:36:57 +0100

    updated project with the javalin example from the course

---
+++ src/main/java/ch/heigvd/dai/App.java
@@ -0,0 +1,50 @@
+package ch.heigvd.dai;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+
+import io.javalin.Javalin;
+import picocli.CommandLine;
+import ch.heigvd.dai.controllers.*;
+import ch.heigvd.dai.models.User;
+
+@CommandLine.Command(description = "CTF manager backend", version = "1.0.0", subcommands = {
+}, scope = CommandLine.ScopeType.INHERIT, mixinStandardHelpOptions = true)
+public class App implements Callable<Integer> {
+
+  @CommandLine.Option(names = { "-a",
+      "--address" }, description = "The address to listen on", defaultValue = "localhost")
+  private String address;
+
+  @CommandLine.Option(names = { "-p",
+      "--port" }, description = "The port to listen on", defaultValue = "8080")
+  private int port;
+
+  @Override
+  public Integer call() throws Exception {
+    Javalin app = Javalin.create(/* config */);
+
+    // TODO: Database
+    ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+
+    // Controllers
+    AuthController authController = new AuthController(users);
+    UsersController usersController = new UsersController(users);
+
+    // Auth routes
+    app.post("/login", authController::login);
+    app.post("/logout", authController::logout);
+    app.get("/profile", authController::profile);
+
+    // Users routes
+    app.post("/users", usersController::create);
+    app.get("/users", usersController::getMany);
+    app.get("/users/{id}", usersController::getOne);
+    app.put("/users/{id}", usersController::update);
+    app.delete("/users/{id}", usersController::delete);
+
+    // TODO: create server config to use the address
+    app.start(port);
+    return 0;
+  }
+}
--- src/main/java/ch/heigvd/dai/Main.java
+++ src/main/java/ch/heigvd/dai/Main.java
@@ -1,23 +1,10 @@
 package ch.heigvd.dai;
 
 import java.io.File;
-import java.util.concurrent.Callable;
 
-import io.javalin.Javalin;
 import picocli.CommandLine;
 
-@CommandLine.Command(description = "A small CLI with subcommands to demonstrate picocli.", version = "1.0.0", subcommands = {
-    // ADD commands
-}, scope = CommandLine.ScopeType.INHERIT, mixinStandardHelpOptions = true)
-public class Main implements Callable<Integer> {
-
-  @CommandLine.Option(names = { "-a",
-      "--address" }, description = "The address to listen on", defaultValue = "localhost")
-  private String address;
-
-  @CommandLine.Option(names = { "-p",
-      "--port" }, description = "The port to listen on when using server mode", defaultValue = "8080")
-  private int port;
+public class Main {
 
   public static void main(String[] args) {
     String jarFilename =
@@ -25,7 +12,7 @@ public class Main implements Callable<Integer> {
         new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath())
             .getName();
 
-    Integer code = new CommandLine(new Main()).setCommandName(jarFilename).execute(args);
+    Integer code = new CommandLine(new App()).setCommandName(jarFilename).execute(args);
 
     if (code != 0) {
       System.exit(code);
@@ -33,11 +20,4 @@ public class Main implements Callable<Integer> {
 
   }
 
-  @Override
-  public Integer call() throws Exception {
-    var app = Javalin.create(/* config */)
-        .get("/", ctx -> ctx.result("Hello World"))
-        .start(port);
-    return 0;
-  }
 }
