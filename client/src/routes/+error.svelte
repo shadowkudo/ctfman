e037140f90d1a61981f1e54816377cc1d2c0278f
Mondotosz <git@moncorp.ch>
2025-01-10 12:34:54 +0100

    added error page

---
+++ client/src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<div class="flex grow items-center justify-center">
+	<div class="flex flex-col items-center gap-4 text-4xl">
+		<h2 class="text-7xl">{page.status}</h2>
+		<h1 class="font-semibold">{page.error?.message}</h1>
+	</div>
+</div>
